84e754abdbc51389fd75f9f36f96a64b320ae7fd
Karter888 <122399969+Karter888@users.noreply.github.com>
2024-06-26 13:56:20 +0200

    save point 2

--- KarterSupplies Website/Frontend/karter-sup/src/components/Navbar.jsx
+++ KarterSupplies Website/Frontend/karter-sup/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {AiOutLineClose, AiOutLineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
    return(
-      <div className='text-yellow-100'>
+      <div className='flex justify-bottom items-center h-24 max-w-[1240px] mx-auto px-4  text-yellow-100'>
          <h1 className= 'w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
       <ul className='flex'>
          <li className='p-4'>Home</li>
@@ -12,6 +13,14 @@ const Navbar = () => {
          <li className='p-4'>About</li>
          <li className='p-4'>Contact</li>
             </ul>
+            <div>
+               <AiOutLineMenu size={20} />
+               <ul>
+               <li className='p-4'>Products</li>
+         <li className='p-4'>About</li>
+         <li className='p-4'>Contact</li>
+               </ul>
+            </div>
       </div>
    )
 }
