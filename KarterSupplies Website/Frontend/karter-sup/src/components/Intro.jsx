dcb0014bdf8e7d688c00ffb2130ba79095424f0a
Karter888 <122399969+Karter888@users.noreply.github.com>
2024-06-29 07:35:21 +0200

    SavePoint 5

--- KarterSupplies Website/Frontend/karter-sup/src/components/Intro.jsx
+++ KarterSupplies Website/Frontend/karter-sup/src/components/Intro.jsx
@@ -4,8 +4,8 @@ import './style.css';
 const Intro = () => {
   return (
     <section className="intro">
-      <h2>About Us</h2>
-      <p>We provide the best supplies for your business needs.</p>
+      <h2>Know About Us</h2>
+      <p>Welcome to Karter Supplies, your premier destination for a vast selection of products and services designed to meet the ever-evolving demands of businesses both in Zambia and around the world. Since our inception in 2019, we have established ourselves as a leading general supplier, unwavering in our commitment to excellence and customer satisfaction.</p>
     </section>
   );
 };
